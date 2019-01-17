d47fc1eabbf5051f0d0407ef3120de47bebe2a73
Nguyen Dinh Quan 20166622 <quan.nd166622@sis.hust.edu.vn>
2019-01-17 13:45:39 +0700

    update ui

---
+++ UIServer/src/main/java/UIServer/UiServerApplication.java
@@ -0,0 +1,14 @@
+package UIServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiServerApplication.class, args);
+	}
+
+}
+
--- UIService/README.md
+++
@@ -1,8 +0,0 @@
-# MISA.SME.java
-
-Signup and signin
-
-need to rebuild database
-need to config auto create database
-need access token
-need +++
